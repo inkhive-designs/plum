5a274abfc8f10421a53351349655a1f9124ce99b
Divjot <divjotdavy@gmail.com>
2018-06-01 14:29:07 +0530

    Links added

--- framework/customizer/misc-scripts.php
+++ framework/customizer/misc-scripts.php
@@ -23,7 +23,8 @@ function plum_customize_register_misc( $wp_customize ) {
                 'description'	=> '<a class="plum-important-links" href="https://inkhive.com/contact-us/" target="_blank">'.__('InkHive Support Forum', 'plum').'</a>
                                     <a class="plum-important-links" href="http://demo.inkhive.com/plum-plus/" target="_blank">'.__('Plum Plus Live Demo', 'plum').'</a>
                                     <a class="plum-important-links" href="https://www.facebook.com/inkhivethemes/" target="_blank">'.__('We Love Our Facebook Fans', 'plum').'</a>
-                                    <a class="plum-important-links" href="https://wordpress.org/support/theme/plum/reviews" target="_blank">'.__('Review Plum on WordPress', 'plum').'</a>',
+                                    <a class="plum-important-links" href="https://wordpress.org/support/theme/plum/reviews" target="_blank">'.__('Review Plum on WordPress', 'plum').'</a>
+                                    <a class="plum-important-links" href="https://inkhive.com/documentation/plum" target="_blank">'.__('Plum Documentation', 'plum').'</a>',
 	            'section' => 'plum_sec_pro',
 	            'settings' => 'plum_pro',			       
 	        )
